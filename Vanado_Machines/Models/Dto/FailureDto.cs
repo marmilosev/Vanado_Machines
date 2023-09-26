88a078ff2cc7773bd8833cd82fe8bb23874f51d0
marmilosev <mmilosevic1411@gmail.com>
2023-09-27 01:38:11 +0200

    Make dto class

---
+++ Vanado_Machines/Models/Dto/FailureDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Vanado_Machines.Models.Dto
+{
+    public class FailureDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int MachineId { get; set; }
+        public string Priority { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+    }
+}
