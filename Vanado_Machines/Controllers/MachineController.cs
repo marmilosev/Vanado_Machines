3c65c8418d2805fa1c342a222c0845f7055a438b
marmilosev <mmilosevic1411@gmail.com>
2023-09-26 22:13:53 +0200

    Create MachineController for get, post, put and delete

---
+++ Vanado_Machines/Controllers/MachineController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Vanado_Machines.Models;
+using Vanado_Machines.Services;
+
+namespace Vanado_Machines.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MachineController : Controller
+    {
+        private readonly IMachineService _machineService;
+        public MachineController(IMachineService machineService)
+        { 
+            _machineService = machineService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetMachines()
+        {
+            var result = await _machineService.GetAllMachines();
+            return Ok(result);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetMachine(int id)
+        {
+            var result = await _machineService.GetMachineById(id);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateMachine([FromBody] Machine machine)
+        {
+            var result = await _machineService.CreateMachine(machine);
+            return Ok(result);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateMachine([FromBody] Machine machine)
+        {
+            var result = await _machineService.UpdateMachine(machine);
+            return Ok(result);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeleteMachine(int id)
+        {
+            var result = await _machineService.DeleteMachine(id);
+            return Ok(result);
+        }
+    }
+}
