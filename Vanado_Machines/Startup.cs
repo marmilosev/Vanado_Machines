0549199ee2e29a55922d3d150c42e8d459739b65
marmilosev <mmilosevic1411@gmail.com>
2023-09-27 14:52:02 +0200

    Added connection with Dapper

---
+++ Vanado_Machines/Startup.cs
@@ -0,0 +1,10 @@
+﻿namespace Vanado_Machines
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.
+        }
+    }
+}
